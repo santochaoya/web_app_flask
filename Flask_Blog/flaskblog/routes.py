3f43cd3f30f4d8fc42e46cf88c8769c1def4be82
Xiao Meng <santochaoya@yahoo.com.hk>
2021-11-22 15:44:06 +0800

    create user with hashed password

--- Flask_Blog/flaskblog/routes.py
+++ Flask_Blog/flaskblog/routes.py
@@ -32,6 +32,8 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         flash(f'Account created for {form.username.data}', category='success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
