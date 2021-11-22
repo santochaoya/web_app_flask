6d0499b32fd8c4ca7efe42b9d70dde9dbe490bf9
Xiao Meng <santochaoya@yahoo.com.hk>
2021-11-22 15:51:56 +0800

    access to database

--- Flask_Blog/flaskblog/routes.py
+++ Flask_Blog/flaskblog/routes.py
@@ -34,6 +34,8 @@ def register():
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}', category='success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
